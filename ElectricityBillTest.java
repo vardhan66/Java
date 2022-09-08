7dcfa70c837d45475c7b0dedf9c0f531f1410383
Eswar <113081338+vardhan66@users.noreply.github.com>
2022-09-09 01:55:40 +0530

    Update ElectricityBillTest.java

--- ElectricityBillTest.java
+++ ElectricityBillTest.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.util.Scanner;//Scanner class is imported to take the manual input.
 class Electricitybill
 {
 	public static void main(String args[])
 	{
-		int t;
-		String cno,cname;
-		double nu,b,b1,pmr,cmr;
-		Scanner s =new Scanner(System.in);
+		int t;//variable of type integer
+		String cno,cname;//variables of type String
+		double nu,b,b1,pmr,cmr;// variables defined of type double 
+		Scanner s =new Scanner(System.in);//object s is created from Scanner class and 'System.in' is used recive input from the user 
 		System.out.println("Enter consumer number");
-		cno=s.nextLine();
+		cno=s.nextLine();//input is stored in cno
 		System.out.println("Enter consumer name");
-		cname=s.nextLine();
+		cname=s.nextLine();//input is stoired in cname
 		System.out.println("Enter previous month reading:");
 		pmr=s.nextDouble();
 		System.out.println("Enter current month reading:");
@@ -19,9 +19,9 @@ class Electricitybill
 			nu=cmr;
 		else
 			nu=cmr-pmr;
-		System.out.println("Choose ypur EB type \n 1.Domestic \n 2.Commercial");
-		t=s.nextInt();
-		EBl e=new EBl();
+		System.out.println("Choose opur EB type \n 1.Domestic \n 2.Commercial");
+		t=s.nextInt();//choice of the user is assignerd to t
+		EBl e=new EBl();//object crerstion from classs EBl
 		b=e.Domestic(nu);
 		b1=e.Commercial(nu);
 		System.out.println("Consumer number"+cno);
