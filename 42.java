6bebe45a927c2a21d9c30d8991f9f91c16fd0ae4
vardhan66 <21bq1a4209@vvit.net>
2022-09-09 02:01:36 +0530

    Prg 4

---
+++ 42.java
@@ -0,0 +1,12 @@
+class BookTest 
+{
+public static void main(String[] args) 
+{
+ Book b=new Book("TVD",2300,"Eswar","JHIM");
+ Book b1=new Book("Greatest Fears",4500,"Benjamin","Franlok");
+ Book b2 =new Book("Kite monster",200,"Sterman","Dual");
+ b.getBookInfo();
+ b1.getBookInfo();
+ b2.getBookInfo();
+}
+}
