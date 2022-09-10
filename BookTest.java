f5e906dfb4f5a53a583e1a01c0cfa7e6b2e88f64
Eswar <113081338+vardhan66@users.noreply.github.com>
2022-09-10 14:03:10 +0530

    Update BookTest.java

--- BookTest.java
+++ BookTest.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-class BookTest 
-{
-public static void main(String[] args) 
-{
- Book b=new Book("TVD",2300,"Eswar","JHIM");
- Book b1=new Book("Greatest Fears",4500,"Benjamin","Franlok");
- Book b2 =new Book("Kite monster",200,"Sterman","Dual");
- b.getBookInfo();
- b1.getBookInfo();
- b2.getBookInfo();
-}
-}
-=======
 class BookTest 
 {
 public static void main(String[] args) 
@@ -24,4 +10,3 @@ public static void main(String[] args)
  b2.getBookInfo();
 }
 }
->>>>>>> fb50a6d9c64cd30c00b4eacd319f6a2123565f10
