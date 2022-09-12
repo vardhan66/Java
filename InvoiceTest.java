b6b87941c7caac9bce32634dba39d11511adddb8
Eswar <113081338+vardhan66@users.noreply.github.com>
2022-09-12 17:07:34 +0530

    Update InvoiceTest.java

--- InvoiceTest.java
+++ InvoiceTest.java
@@ -4,17 +4,24 @@ class InvoiceTest
 {
      public static void main(String Var[]) {//starting of the main method
         Scanner s = new Scanner(System.in);//creating scanner object
+          
         System.out.println("Enter Part Number:");//partnumber prompt
         String pno=s.nextLine();//reads the input and cursor goes to next line
+          
         System.out.println("Enter Part Description:");//PROMPT TO Enter part description
         String pdes=s.nextLine();//reads the String input and the cursor goes to nextline
+          
         System.out.println("Enter Quantitiy:");//prompt to enter quantity
         int q = s.nextInt();//reads the integer input
+          
         System.out.println("Enter Price:");//prompt to enter the price 
         double p=s.nextDouble();//reads the price input
+          
         //Creating an object of Invoice class
         Invoice o = new Invoice(pno, pdes, q, p);
         double total=o.getInvoiceAmount();//assigning the caluculated amount to total
+          
+          
         //To display the details:
         System.out.println("Part Number:"+o.getPartNum());
         System.out.println("Part Description:"+o.getPartDeS());
