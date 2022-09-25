b4ce597fbabf8f2fff5a0153669a1ffbf59ce735
Eswar <21bq1a4209@vvit.net>
2022-09-25 20:51:32 +0530

    Demo

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        char a = 88;
+        char b = 86;
+        System.out.println(a);
+        System.out.println(b
+        
+    );
+
+    }
+}
